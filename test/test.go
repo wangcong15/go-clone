cff8702fc3d08dbb2aa4608010f5869f7db832af
wangcong15 <wangcong15@mails.tsinghua.edu.cn>
2018-08-03 19:33:34 +0800

    cfg module

---
+++ test/test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"go-clone/cfg"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+func main() {
+	fmt.Println("Hello Test")
+	// source code of golang function
+	src := `package main
+			func hahah() {
+				if x := f(); x != nil {
+					T()
+				} else {
+					F()
+				}
+			}
+			func f() int {
+				return 0
+			}
+			func T() {
+				
+			}
+			func F() {
+
+			}
+			`
+
+	// Create the AST by parsing src.
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	ast.Inspect(file, func(x ast.Node) bool {
+		switch n := x.(type) {
+			case *ast.FuncDecl:
+				fmt.Println("--- ", n.Name ," ---")
+				fmt.Println(n.Body)
+				cfg_data := cfg.New(n.Body, mayReturn)
+				fmt.Println(cfg_data)
+				fmt.Println(cfg_data.Format(fset))
+		}
+		return true
+	})
+}
+
+func mayReturn(ce *ast.CallExpr) bool {
+	return true
+}
